7598325a26d37880de8aae50cb2937c9a97f7874
Ana Beatriz Costa Viana <contatobiaviana@gmail.com>
2023-02-09 08:11:24 -0300

    Setup

---
+++ taskmanager/src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div className="title">ReciclaClub Task Manager</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
